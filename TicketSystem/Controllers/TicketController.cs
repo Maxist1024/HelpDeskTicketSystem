888045ee0d0f83ee4fb538a9cf6add910b6e78a8
PiotrGryglicki <p.gryglicki@gmail.com>
2018-06-06 19:16:04 +0200

    Dodanie TicketController i widoku formularza do zglaszania ticketu.

---
+++ TicketSystem/Controllers/TicketController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using TicketSystem.DataAccess;
+
+namespace TicketSystem.Controllers
+{
+    public class TicketController : Controller
+    {
+
+        private TicketDbContext _context;
+
+        public TicketController()
+        {
+            _context = TicketDbContext.Create();
+        }
+
+
+        // GET: Zwaraca formularz do wypełnienia zgłoszenia
+        // [Authorize]
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
